a03589ea4ce26bd8a7a558dbe80c7e7cd79b38f8
Kishore Kumaar Natarajan <kkumaarn@amazon.com>
2025-04-01 14:48:43 -0700

    Removed Creating Template
    Signed-off-by: Kishore Kumaar Natarajan <kkumaarn@amazon.com>

--- src/test/java/org/opensearch/plugin/insights/core/exporter/QueryInsightsExporterIT.java
+++ src/test/java/org/opensearch/plugin/insights/core/exporter/QueryInsightsExporterIT.java
@@ -201,8 +201,9 @@ public class QueryInsightsExporterIT extends QueryInsightsRestTestCase {
     private void cleanup(String fullIndexName) throws IOException, InterruptedException {
         Thread.sleep(3000);
 
+
         try {
-            client().performRequest(new Request("DELETE", "/" + fullIndexName));
+            client().performRequest(new Request("DELETE", "/top_queries"));
         } catch (ResponseException ignored) {}
 
         try {
