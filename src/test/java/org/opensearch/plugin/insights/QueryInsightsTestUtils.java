54568e902feb08cf3a4429b3fd0200910b259367
Emily Guo <35637792+LilyCaroline17@users.noreply.github.com>
2026-01-15 21:10:50 -0800

    Add source truncated attribute to QueryInsightsTestUtils
    Signed-off-by: Emily Guo <35637792+LilyCaroline17@users.noreply.github.com>

--- src/test/java/org/opensearch/plugin/insights/QueryInsightsTestUtils.java
+++ src/test/java/org/opensearch/plugin/insights/QueryInsightsTestUtils.java
@@ -166,6 +166,7 @@ final public class QueryInsightsTestUtils {
             Map<Attribute, Object> attributes = new HashMap<>();
             attributes.put(Attribute.SEARCH_TYPE, SearchType.QUERY_THEN_FETCH.toString().toLowerCase(Locale.ROOT));
             attributes.put(Attribute.SOURCE, new SourceString(searchSourceBuilder.toString()));
+            attributes.put(Attribute.SOURCE_TRUNCATED, false);
             attributes.put(Attribute.TOTAL_SHARDS, randomIntBetween(1, 100));
             attributes.put(Attribute.INDICES, randomArray(1, 3, Object[]::new, () -> randomAlphaOfLengthBetween(5, 10)));
             attributes.put(Attribute.PHASE_LATENCY_MAP, phaseLatencyMap);
