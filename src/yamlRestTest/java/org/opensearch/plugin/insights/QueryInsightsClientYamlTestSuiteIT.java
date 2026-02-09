a5b689ce2472af17f2e9d3a21747b779764cdf0d
Igor Motov <igor@motovs.org>
2026-02-09 09:54:32 -1000

    Enable internalClusterTest and yamlRestTest tasks (#522)
    * Enable internalClusterTest and yamlRestTest tasks
    
    Closes #514
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    
    * Fix integTestWithSecurity
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    
    * Improve internal cluster test resiliency
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    
    * Improve internal cluster test resiliency, 2nd attempt
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    
    * Remove changes in deleteExpiredTopNIndices
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    
    * Fix incorrect package name
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    
    ---------
    
    Signed-off-by: Igor Motov <igor@motovs.org>
    Co-authored-by: Chenyang Ji <cyji@amazon.com>

--- src/yamlRestTest/java/org/opensearch/plugin/insights/QueryInsightsClientYamlTestSuiteIT.java
+++ src/yamlRestTest/java/org/opensearch/plugin/insights/QueryInsightsClientYamlTestSuiteIT.java
@@ -5,7 +5,7 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
-package org.opensearch.path.to.plugin;
+package org.opensearch.plugin.insights;
 
 import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
